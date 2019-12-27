2d782a3c3394bcfc47d84ce5190b73dace944662
Marcelo Emiliano <celoemiliano22@yahoo.com.br>
2019-12-26 21:13:35 -0800

    Testando PSR

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php echo phpinfo();
+<?php
+namespace MeuSistema;
+
+class MinhaClasse
+{
+    public static function imprimirInfo()
+    {
+        echo phpinfo();
+    }
+}
+MinhaClasse::imprimirInfo();
